36299a36a34f5703c0b2e3ad263a771a1b58df01
watssupp <jorgesolisruiz@gmal.com>
2022-11-03 11:18:20 -0500

    set up package.json with dependances, started making models

---
+++ models/Reaction.js
@@ -0,0 +1,25 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max_length: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+);
+
+module.exports = reactionSchema;
---
+++ models/Thought.js
@@ -0,0 +1,29 @@
+const { Schema,model } = require("mongoose");
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema (
+    {
+        thoughtText:{
+            type: String,
+            required: true,
+            maxlength:280,
+        },
+        createdAt:{
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    }
+)
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('thought',thoughtSchema);
+
+module.exports = Thought
